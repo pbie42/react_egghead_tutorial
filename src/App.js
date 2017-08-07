8d9742f0766aa5fcc74dcea5a0fb4eb0abe97ce0
Paul Bie <synergized@iMac-de-Synergized.local>
2017-08-07 19:15:18 +0200

    [Add] - delete and toggle functions now influence the DB

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { TodoForm, TodoList, Footer } from './components/todo'
 import { pipe, partial } from './lib/utils'
 import { addTodo, generateId, findById, toggleTodo, updateTodo, removeTodo, filterTodos } from './lib/todoHelpers'
 import { string } from 'prop-types'
-import { loadTodos } from './lib/todoService'
+import { loadTodos, createTodo, saveTodo, destroyTodo } from './lib/todoService'
 
 class App extends Component {
 
@@ -27,12 +27,18 @@ class App extends Component {
     evt.preventDefault()
     const updatedTodos = removeTodo(this.state.todos, id)
     this.setState({ todos: updatedTodos })
+    destroyTodo(id)
+      .then(() => this.showTempMessage('Todo Removed'))
   }
 
   handleToggle = (id) => {
-    const getUpdatedTodos = pipe(findById, toggleTodo, partial(updateTodo, this.state.todos))
-    const updatedTodos = getUpdatedTodos(id, this.state.todos)
+    const getToggledTodo = pipe(findById, toggleTodo)
+    const updated = getToggledTodo(id, this.state.todos)
+    const getUpdatedTodos = partial(updateTodo, this.state.todos)
+    const updatedTodos = getUpdatedTodos(updated)
     this.setState({ todos: updatedTodos })
+    saveTodo(updated)
+      .then(() => this.showTempMessage('Todo Updated'))
   }
 
   handleSubmit = (evt) => {
@@ -45,6 +51,13 @@ class App extends Component {
       currentTodo: '',
       errorMessage: ''
     })
+    createTodo(newTodo)
+      .then(() => this.showTempMessage('Todo Added'))
+  }
+
+  showTempMessage = (msg) => {
+    this.setState({ message: msg })
+    setTimeout(() => this.setState({ message: '' }), 2500)
   }
 
   handleEmptySubmit = (evt) => {
@@ -71,6 +84,7 @@ class App extends Component {
         </div>
         <div className="Todo-App">
           { this.state.errorMessage && <span className="error">{ this.state.errorMessage }</span>}
+          { this.state.message && <span className="success">{ this.state.message }</span>}
           <TodoForm handleInputChange={ this.handleInputChange } currentTodo={ this.state.currentTodo } handleSubmit={ submitHandler }/>
           <TodoList handleToggle={ this.handleToggle } handleRemove={ this.handleRemove } todos={ displayTodos }/>
           <Footer/>
